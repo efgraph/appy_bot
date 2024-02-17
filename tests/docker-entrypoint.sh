808b88926fc4fb8d10eabbc811c4db6c5467aa3d
dencheek <d.spiridonov@lipt-soft.ru>
2024-02-18 02:03:02 +0300

    add tests flake

--- tests/docker-entrypoint.sh
+++ tests/docker-entrypoint.sh
@@ -2,5 +2,7 @@
 ./wait-for-it.sh postgres:5432 -t 10 -- echo "postgres is up"
 alembic upgrade head
 sleep 2
-python3 -m pytest --cov=bot -p no:warnings
+python -m coverage run -m pytest tests -p no:warnings
+python -m coverage report
 python3 -m flake8 bot
+python3 -m flake8 tests
